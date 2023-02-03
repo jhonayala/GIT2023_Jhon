b79607e4cf8c41e0e18595f6c9f039c22565d4f7
jhon <a_jhondeivis.ayala.braithwite@iespablopicasso.es>
2023-02-03 13:20:37 +0100

    Creo la clase vehiculo.java con sus respectivos parametros

---
+++ vehiculo.java
@@ -0,0 +1,27 @@
+
+public class vehiculo {
+    private String marca;
+    private String modelo;
+    private String matricula;
+
+    public String getMarca() {
+        return marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    
+    public vehiculo (String marca, String modelo, String matricula) {
+        this.marca=marca;
+        this.modelo=modelo;
+        this.matricula=matricula;
+        
+    }
+    
+
+
+    
+}
